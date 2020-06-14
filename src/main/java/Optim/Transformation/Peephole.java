bda5f5bcb747b1d2a810727ef5f47aa49b73fed7
yujie6 <yujie6@sjtu.edu.cn>
2020-06-14 16:52:38 +0800

    add peephole

--- src/main/java/Optim/Transformation/Peephole.java
+++ src/main/java/Optim/Transformation/Peephole.java
@@ -3,7 +3,10 @@ package Optim.Transformation;
 import IR.BasicBlock;
 import IR.Function;
 import IR.Instructions.Instruction;
+import IR.Instructions.LoadInst;
+import IR.Instructions.StoreInst;
 import Optim.FunctionPass;
+import Optim.MxOptimizer;
 
 public class Peephole extends FunctionPass {
 
@@ -17,11 +20,41 @@ public class Peephole extends FunctionPass {
     public boolean optimize() {
         elimNum = 0;
         for (BasicBlock BB : function.getBlockList()) {
-            for (int i = 1; i < BB.getInstList().size(); i++) {
-                Instruction inst = BB.getInstList().get(i);
-                Instruction prev = BB.getInstList().get(i-1);
+            boolean changed = false;
+            do {
+                changed = false;
+                int len = BB.getInstList().size();
+                for (int i = 0; i < len; i++) {
+                    Instruction inst = BB.getInstList().get(i);
+                    if (inst instanceof StoreInst) {
+                        LoadInst elimLoad = null;
+                        for (int j = 1; i + j < Math.min(i + 14, len); j++) {
+                            Instruction elim = BB.getInstList().get(i + j);
+                            if (elim instanceof StoreInst) {
+                                break;
+                            } else if (elim instanceof LoadInst) {
+                                if (((LoadInst) elim).getLoadAddr() == ((StoreInst) inst).getStoreDest()) {
+                                    elimLoad = ((LoadInst) elim);
+                                    break;
+                                }
+                            }
+                        }
+                        if (elimLoad != null) {
+                            elimNum += 1;
+                            changed = true;
+                            elimLoad.replaceAllUsesWith(((StoreInst) inst).getStoreValue());
+                            elimLoad.eraseFromParent();
+                            break;
+                        }
+                    }
+                }
+            } while (changed);
 
-            }
+        }
+
+        if (elimNum != 0) {
+            MxOptimizer.logger.fine(String.format("Peephole works on function \"%s\", with %d insts eliminated",
+                    function.getIdentifier(), elimNum));
         }
 
         return elimNum != 0;
